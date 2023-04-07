8f7b593a15fb99953dbf8461ad76dd625739f81a
aishwarya0009 <55315263+aishwarya0009@users.noreply.github.com>
2023-04-07 15:54:23 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,117 @@
-import React from 'react'
+
+import React, { useState } from 'react';
 import '../styles/App.css';
 
 const App = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    contactNo: '',
+  });
+  const [successAlert, setSuccessAlert] = useState(false);
+  const [usernameError, setUsernameError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [contactNoError, setContactNoError] = useState('');
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const isValid = validateForm();
+    if (isValid) {
+      setSuccessAlert(true);
+      setFormData({
+        username: '',
+        email: '',
+        password: '',
+        contactNo: '',
+      });
+      setTimeout(() => {
+        setSuccessAlert(false);
+      }, 2000);
+    }
+  };
+
+  const validateForm = () => {
+    let isValid = true;
+    if (!formData.username) {
+      setUsernameError('Please enter a username');
+      isValid = false;
+    } else {
+      setUsernameError('');
+    }
+    if (!formData.email) {
+      setEmailError('Please enter an email');
+      isValid = false;
+    } else {
+      setEmailError('');
+    }
+    if (!formData.password || formData.password.length < 4) {
+      setPasswordError('Password must be at least 4 characters long');
+      isValid = false;
+    } else {
+      setPasswordError('');
+    }
+    if (!formData.contactNo || formData.contactNo.toString().length !== 10) {
+      setContactNoError('Contact number should have exactly 10 digits');
+      isValid = false;
+    } else {
+      setContactNoError('');
+    }
+    return isValid;
+  };
 
   return (
     <div id="main">
-      {/* {<h3 className='success-alert'>Registered Successfullly</h3>} */}
-      <form>
-        <h1>Registeration Form</h1>
+      {successAlert && (
+        <h3 className='success-alert'>Registered Successfully</h3>
+      )}
+      <form onSubmit={handleSubmit}>
+        <h1>Registration Form</h1>
         <section>
           <label>Username</label>
-          <input type="text" name='username' />
-          {/* <p className='username-error'></p> */}
+          <input
+            type="text"
+            name='username'
+            value={formData.username}
+            onChange={handleInputChange}
+          />
+          <p className='username-error'>{usernameError}</p>
           <label>Email</label>
-          <input type="email" name='email' />
-          {/* <p className='email-error'></p> */}
+          <input
+            type="email"
+            name='email'
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+          <p className='email-error'>{emailError}</p>
           <label>Password</label>
-          <input type="password" name='password' />
-          {/* <p className='password-error'></p> */}
+          <input
+            type="password"
+            name='password'
+            value={formData.password}
+            onChange={handleInputChange}
+          />
+          <p className='password-error'>{passwordError}</p>
           <label>Contact Number</label>
-          <input type="number" name='contactNo' />
-          {/* <p className='contactNo-error'></p> */}
-          <button>Submit</button>
+          <input
+            type="number"
+            name='contactNo'
+            value={formData.contactNo}
+            onChange={handleInputChange}
+          />
+          <p className='contactNo-error'>{contactNoError}</p>
+          <button type='submit'>Submit</button>
         </section>
       </form>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
+
