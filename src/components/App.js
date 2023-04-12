5222362ef724399211136aaac227bfcb4e2ddaf1
aishwarya0009 <55315263+aishwarya0009@users.noreply.github.com>
2023-04-12 20:53:29 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,117 +1,141 @@
+import React, { useState } from 'react'
 
-import React, { useState } from 'react';
 import '../styles/App.css';
 
 const App = () => {
-  const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: '',
-    contactNo: '',
-  });
-  const [successAlert, setSuccessAlert] = useState(false);
-  const [usernameError, setUsernameError] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [passwordError, setPasswordError] = useState('');
-  const [contactNoError, setContactNoError] = useState('');
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
-  };
+const [formFields, setFormFields] = useState({
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const isValid = validateForm();
-    if (isValid) {
-      setSuccessAlert(true);
-      setFormData({
-        username: '',
-        email: '',
-        password: '',
-        contactNo: '',
-      });
-      setTimeout(() => {
-        setSuccessAlert(false);
-      }, 2000);
-    }
-  };
+username: "",
 
-  const validateForm = () => {
-    let isValid = true;
-    if (!formData.username) {
-      setUsernameError('Please enter a username');
-      isValid = false;
-    } else {
-      setUsernameError('');
-    }
-    if (!formData.email) {
-      setEmailError('Please enter an email');
-      isValid = false;
-    } else {
-      setEmailError('');
-    }
-    if (!formData.password || formData.password.length < 4) {
-      setPasswordError('Password must be at least 4 characters long');
-      isValid = false;
-    } else {
-      setPasswordError('');
-    }
-    if (!formData.contactNo || formData.contactNo.toString().length !== 10) {
-      setContactNoError('Contact number should have exactly 10 digits');
-      isValid = false;
-    } else {
-      setContactNoError('');
-    }
-    return isValid;
-  };
+email: "",
 
-  return (
-    <div id="main">
-      {successAlert && (
-        <h3 className='success-alert'>Registered Successfully</h3>
-      )}
-      <form onSubmit={handleSubmit}>
-        <h1>Registration Form</h1>
-        <section>
-          <label>Username</label>
-          <input
-            type="text"
-            name='username'
-            value={formData.username}
-            onChange={handleInputChange}
-          />
-          <p className='username-error'>{usernameError}</p>
-          <label>Email</label>
-          <input
-            type="email"
-            name='email'
-            value={formData.email}
-            onChange={handleInputChange}
-          />
-          <p className='email-error'>{emailError}</p>
-          <label>Password</label>
-          <input
-            type="password"
-            name='password'
-            value={formData.password}
-            onChange={handleInputChange}
-          />
-          <p className='password-error'>{passwordError}</p>
-          <label>Contact Number</label>
-          <input
-            type="number"
-            name='contactNo'
-            value={formData.contactNo}
-            onChange={handleInputChange}
-          />
-          <p className='contactNo-error'>{contactNoError}</p>
-          <button type='submit'>Submit</button>
-        </section>
-      </form>
-    </div>
-  );
-};
+password: "",
+
+contactNo: ""
+
+});
+
+const [formErrors, setFormErrors] = useState({});
+
+const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+
+function handleInputChange(event) {
+
+const { name, value } = event.target;
+
+setFormFields({...formFields, [name]: value});
+
+}
+
+function validateForm(inputValues) {
+
+const errors = {};
+
+if (!inputValues.username) {
+
+errors.username = "Username is required";
+
+}
+
+if (!inputValues.email) {
+
+errors.email = "Email is required";
+
+}
+
+if (!inputValues.password) {
+
+errors.password = "Password is required";
+
+} else if (inputValues.password.length < 4) {
+
+errors.password = "Password must have more than 4 characters";
+
+}
+
+if (!inputValues.contactNo) {
+
+errors.contactNo = "Contact no. is required";
+
+} else if (inputValues.contactNo.length !== 10) {
+
+errors.contactNo = "Invalid contact no.";
+
+}
+
+return errors;
+
+}
+
+function handleFormSubmit(event) {
+
+event.preventDefault();
+
+setFormErrors(validateForm(formFields));
+
+setIsFormSubmitted(true);
+
+setFormFields({
+
+username: "",
+
+email: "",
+
+password: "",
+
+contactNo: ""
+
+})
+
+}
+
+return (
+
+<div id="main">
+
+{Object.keys(formErrors).length === 0 && isFormSubmitted ? <h3 className='success-alert'>Registered Successfullly</h3> : null}
+
+<form onSubmit={handleFormSubmit}>
+
+<h1>Registeration Form</h1>
+
+<section>
+
+<label>Username</label>
+
+<input type="text" name='username' value={formFields.username} onChange={handleInputChange} />
+
+{formErrors.username && <p className='username-error'>{formErrors.username}</p>}
+
+<label>Email</label>
+
+<input type="email" name='email' value={formFields.email} onChange={handleInputChange} />
+
+{formErrors.email && <p className='email-error'>{formErrors.email}</p>}
+
+<label>Password</label>
+
+<input type="password" name='password' value={formFields.password} onChange={handleInputChange} />
+
+{formErrors.password && <p className='password-error'>{formErrors.password}</p>}
+
+<label>Contact Number</label>
+
+<input type="number" name='contactNo' value={formFields.contactNo} onChange={handleInputChange} />
+
+{formErrors.contactNo && <p className='contactNo-error'>{formErrors.contactNo}</p>}
+
+<button onClick={handleFormSubmit}>Submit</button>
+
+</section>
+
+</form>
+
+</div>
+
+)
+
+}
 
 export default App;
-
